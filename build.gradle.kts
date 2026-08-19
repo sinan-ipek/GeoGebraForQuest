67ee5f21334f5ec0b08a113d6214446389cb8fef
sinan-ipek <116756651+sinan-ipek@users.noreply.github.com>
2026-08-19 02:07:03 +0200

    Match Meta sample root plugin classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.meta.spatial.plugin) apply false
 }
