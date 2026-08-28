82dfa16afc42b163c553ab4486e46126361e91e8
sinan-ipek <116756651+sinan-ipek@users.noreply.github.com>
2026-08-28 18:48:25 +0200

    PC v0.1: add application entry point

---
+++ pc/Program.cs
@@ -0,0 +1,13 @@
+namespace GeoGebraForQuest.PC;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainForm());
+    }
+}
